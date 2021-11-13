8077b7466b9b3e1c1df32178c909f8663dff9166
izzkhairable <izzkhair@gmail.com>
2021-11-14 05:18:54 +0800

    Main Shell Script

--- shell_script.sh
+++ shell_script.sh
@@ -6,12 +6,12 @@ yum install git -y
 yum install gcc -y
 yum install build-essential -y
 yum install python3-pip python3-devel python3-setuptools -y
-yum install -y mysql-devel <-----Extra packages you need, if not remove it.
+yum install -y mysql-devel # Extra packages you need, if not remove it.
 mkdir ~/.aws
-echo -e "Your access id and secret to aws if needed." >> ~/.aws/config
+echo -e "Your access id and secret to aws, if needed e.g. accessing S3 Bucket." >> ~/.aws/config
 mkdir /var/www
 cd /var/www
-git clone https://your/git/repo.git /var/www
+git clone https://your/git/repo.git /var/www #If private repo please SSH instead.
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
